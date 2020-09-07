1ef11ed9a0ef39dc66070aaf76206d56b1caeeb0
Linus Zheng <linus.zheng@warriorlife.net>
2020-09-07 10:33:32 -0700

    added subpages

--- script.js
+++ script.js
@@ -20,26 +20,48 @@ var db = firebase.database();
 
 // --------------------------------- Other setup ----------------------------------
 
-h_group = document.getElementById("group");
-h_test = document.getElementById("test-btn");
+var h_group = document.getElementById("group");
+var h_test = document.getElementById("test-btn");
+
+var new_window;
 
 // ---------------------------------- Functions --------------------------------
 
-function test() {
+function testDb() {
+	var today = new Date();
+	var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
 	db.ref('tabs/' + 'asdfljk23423adfslk').set({
-		name: "learn@vcs",
-		url: "learn.vcs.net"
+		name: "curTime",
+		url: time
 	});
+}
+
+function testHash() {
+	if(window.location.hash){
+		var g = window.location.hash.substring(1);
+		h_group.innerHTML = g;
+	}
 }
 
 // ----------------------------------- Testing -------------------------------------
 
-if(window.location.hash){
-	var g = window.location.hash.substring(1);
-	h_group.innerHTML = g;
-}
 
-h_test.addEventListener("click", test, false);
+h_test.addEventListener("click", testHash, false);
+
+
+// setTimeout(function(){
+// 	new_window = window.open("https://stackoverflow.com/questions/7064998/how-to-make-a-link-open-multiple-pages-when-clicked");
+// }, 10000);
+
+
+// setTimeout(function(){
+// 	new_window.close()
+// }, 20000);
+
+
+
+
+
 
 
 
